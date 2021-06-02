5d6b837b37e2d953a91832adcbec8c46ab1db56e
ikeohachidi <ikeohachidi@gmail.com>
2021-06-03 00:36:11 +0100

    feat: update modal positioning

--- src/components/Modal/Modal.vue
+++ src/components/Modal/Modal.vue
@@ -1,5 +1,5 @@
 <template>
-    <section class="bg-black bg-opacity-80 fixed flex items-center justify-center top-0 w-screen h-screen">
+    <section class="bg-black bg-opacity-80 flex items-center justify-center fixed top-0 left-0 w-screen h-screen z-10">
         <div class="bg-white overflow-hidden rounded-lg w-2/6">
             <slot name="title"></slot>
             <p class="text-lg p-4 font-bold border-b border-gray-300 text-gray-700" v-if="title">{{ title }}</p>
