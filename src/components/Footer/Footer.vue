61c8a0c34769fa8d16fd3932d735535c0fd41f79
ikeohachidi <ikeohachidi@gmail.com>
2021-07-03 21:04:37 +0100

    style: update footer colors

--- src/components/Footer/Footer.vue
+++ src/components/Footer/Footer.vue
@@ -9,12 +9,12 @@ import {Vue, Component} from 'vue-property-decorator';
 
 @Component
 export default class Footer extends Vue {
-    
+
 }
 </script>
 
 <style lang="scss" scoped>
 footer {
-    @apply bg-gray-100 text-gray-700 py-6;
+    @apply bg-gray-100 text-gray-700 py-6 mt-9;
 }
 </style>
