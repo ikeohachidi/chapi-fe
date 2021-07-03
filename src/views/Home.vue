85c3692ff26a0bd821eda07081841be2d90ae20e
ikeohachidi <ikeohachidi@gmail.com>
2021-07-03 22:33:04 +0100

    feat: wire up get started button

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -5,7 +5,7 @@
 				<h1 class="text-2xl md:text-4xl font-bold mb-10">A simple and painless proxy for you JAMSTACK application</h1>
 				<p class="mb-6">Chapi is a simple application aimed at the JAMSTACK or frontend developer who wants to access some api’s but doesn’t want to have to spin up an entire backend to hide API keys or doesn’t want to have to write complex cloud functions. It’s quick and easy to setup and get going!!</p>
 
-				<button>Get started</button>
+				<button @click="routeToDashboard">Get started</button>
 			</div>
 			<div class="hidden md:block">
 				<img src="@/assets/landing-page.svg" alt="Landing page" class="h-4/5 ml-auto">
@@ -41,6 +41,9 @@ import {Vue, Component} from 'vue-property-decorator';
 
 @Component
 export default class Home extends Vue {
+	private routeToDashboard() {
+		this.$router.push({ name: 'Dashboard' })
+	}
 }
 </script>
 
